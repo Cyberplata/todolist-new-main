110d2984f189f3d62eaf4d357f28ab3e31857847
PlatonKynin <kininplaton@gmail.com>
2025-05-13 00:16:23 +0300

    4 Спринт 3 неделя - Домашнее задание: 2. Disable EditableSpan - остановился на дизейбле. Решение добавил описанное v1

--- src/features/todolists/model/tasks-slice.ts
+++ src/features/todolists/model/tasks-slice.ts
@@ -1,6 +1,6 @@
 import { setAppStatusAC } from "@/app/app-slice.ts"
-import type { RootState } from "@/app/store.ts"
 import { ResultCode } from "@/common/enums/enums.ts"
+import type { RequestStatus } from "@/common/types"
 import { createAppSlice, handleServerAppError, handleServerNetworkError } from "@/common/utils"
 import { tasksApi } from "@/features/todolists/api/tasksApi.ts"
 import type { DomainTask, UpdateTaskModel } from "@/features/todolists/api/tasksApi.types.ts"
@@ -29,7 +29,9 @@ export const tasksSlice = createAppSlice({
       },
       {
         fulfilled: (state, action) => {
-          state[action.payload.todolistId] = action.payload.tasks
+          // state[action.payload.todolistId] = action.payload.tasks
+          if (!action.payload) return
+          state[action.payload.todolistId] = action.payload.tasks.map((t) => ({ ...t, entityStatus: "idle" }))
         },
       },
     ),
@@ -54,7 +56,11 @@ export const tasksSlice = createAppSlice({
       },
       {
         fulfilled: (state, action) => {
-          const newTask: DomainTask = action.payload.task
+          // const newTask: DomainTask = action.payload.task
+          const newTask: TaskWithStatus = {
+            ...action.payload.task,
+            entityStatus: "idle",
+          }
           state[newTask.todoListId].unshift(newTask)
         },
       },
@@ -89,100 +95,111 @@ export const tasksSlice = createAppSlice({
         },
       },
     ),
-    // updateTaskTC: create.asyncThunk(
-    //   async (task: DomainTask, thunkAPI) => {
-    //     const { todoListId, id, description, title, priority, startDate, deadline, status } = task
-    //     const { rejectWithValue, dispatch } = thunkAPI
-    //
-    //     const model: UpdateTaskModel = {
-    //       description,
-    //       title,
-    //       priority,
-    //       startDate,
-    //       deadline,
-    //       status,
-    //     }
-    //
-    //     try {
-    //       dispatch(setAppStatusAC({ status: "loading" }))
-    //       dispatch(changeTodolistStatusAC({ id, entityStatus: "loading" }))
-    //       const res = await tasksApi.updateTask({ todolistId: todoListId, taskId: id, model })
-    //       if (res.data.resultCode === ResultCode.Success) {
-    //         dispatch(setAppStatusAC({ status: "succeeded" }))
-    //         const newTask = res.data.data.item
-    //         return { task: newTask }
-    //       } else {
-    //         // 200 проверка errors
-    //         handleServerAppError(res.data, dispatch)
-    //         return rejectWithValue(null)
-    //       }
-    //     } catch (error) {
-    //       // 400 - 500 errors
-    //       handleServerNetworkError(dispatch, error)
-    //       return rejectWithValue(null)
-    //     }
-    //   },
-    //   {
-    //     fulfilled: (state, action) => {
-    //       const { title, status, todoListId, id } = action.payload.task
-    //       const task = state[todoListId].find((task) => task.id === id)
-    //       if (task) {
-    //         task.title = title
-    //         task.status = status
-    //       }
-    //     },
-    //   },
-    // ),
     updateTaskTC: create.asyncThunk(
-      async (
-        payload: { todolistId: string; taskId: string; domainModel: Partial<UpdateTaskModel> },
-        { dispatch, getState, rejectWithValue },
-      ) => {
-        const { todolistId, taskId, domainModel } = payload
-
-        const allTodolistTasks = (getState() as RootState).tasks[todolistId]
-        const task = allTodolistTasks.find((task) => task.id === taskId)
-
-        if (!task) {
-          return rejectWithValue(null)
-        }
+      async (task: TaskWithStatus, thunkAPI) => {
+        const { todoListId, id, description, title, priority, startDate, deadline, status } = task
+        const { rejectWithValue, dispatch } = thunkAPI
 
         const model: UpdateTaskModel = {
-          description: task.description,
-          title: task.title,
-          priority: task.priority,
-          startDate: task.startDate,
-          deadline: task.deadline,
-          status: task.status,
-          ...domainModel,
+          description,
+          title,
+          priority,
+          startDate,
+          deadline,
+          status,
         }
 
         try {
           dispatch(setAppStatusAC({ status: "loading" }))
-          // dispatch(changeTodolistStatusAC({ id: taskId, entityStatus: "loading" }))
-          const res = await tasksApi.updateTask({ todolistId, taskId, model })
+          dispatch(changeTaskEntityStatusAC({ todolistId: todoListId, taskId: id, entityStatus: "loading" }))
+          const res = await tasksApi.updateTask({ todolistId: todoListId, taskId: id, model })
           if (res.data.resultCode === ResultCode.Success) {
             dispatch(setAppStatusAC({ status: "succeeded" }))
-            return { task: res.data.data.item }
+            const newTask = res.data.data.item
+            return { task: newTask }
           } else {
-            // dispatch(changeTodolistStatusAC({ id: todolistId, entityStatus: "idle" }))
+            // 200 проверка errors
             handleServerAppError(res.data, dispatch)
             return rejectWithValue(null)
           }
         } catch (error) {
-          // dispatch(changeTodolistStatusAC({ id: todolistId, entityStatus: "idle" }))
+          // 400 - 500 errors
           handleServerNetworkError(dispatch, error)
           return rejectWithValue(null)
         }
       },
       {
         fulfilled: (state, action) => {
-          const allTodolistTasks = state[action.payload.task.todoListId]
-          const taskIndex = allTodolistTasks.findIndex((task) => task.id === action.payload.task.id)
-          if (taskIndex !== -1) {
-            allTodolistTasks[taskIndex] = action.payload.task
+          const { title, status, todoListId, id } = action.payload.task
+          const task = state[todoListId].find((task) => task.id === id)
+          if (task) {
+            task.title = title
+            task.status = status
           }
         },
+      },
+    ),
+
+    // updateTaskTC: create.asyncThunk(
+    //   async (
+    //     payload: { todolistId: string; taskId: string; domainModel: Partial<UpdateTaskModel> },
+    //     { dispatch, getState, rejectWithValue },
+    //   ) => {
+    //     const { todolistId, taskId, domainModel } = payload
+    //
+    //     const allTodolistTasks = (getState() as RootState).tasks[todolistId]
+    //     const task = allTodolistTasks.find((task) => task.id === taskId)
+    //
+    //     if (!task) {
+    //       return rejectWithValue(null)
+    //     }
+    //
+    //     const model: UpdateTaskModel = {
+    //       description: task.description,
+    //       title: task.title,
+    //       priority: task.priority,
+    //       startDate: task.startDate,
+    //       deadline: task.deadline,
+    //       status: task.status,
+    //       ...domainModel,
+    //     }
+    //
+    //     try {
+    //       dispatch(setAppStatusAC({ status: "loading" }))
+    //       // dispatch(changeTodolistStatusAC({ id: taskId, entityStatus: "loading" }))
+    //       const res = await tasksApi.updateTask({ todolistId, taskId, model })
+    //       if (res.data.resultCode === ResultCode.Success) {
+    //         dispatch(setAppStatusAC({ status: "succeeded" }))
+    //         return { task: res.data.data.item }
+    //       } else {
+    //         // dispatch(changeTodolistStatusAC({ id: todolistId, entityStatus: "idle" }))
+    //         handleServerAppError(res.data, dispatch)
+    //         return rejectWithValue(null)
+    //       }
+    //     } catch (error) {
+    //       // dispatch(changeTodolistStatusAC({ id: todolistId, entityStatus: "idle" }))
+    //       handleServerNetworkError(dispatch, error)
+    //       return rejectWithValue(null)
+    //     }
+    //   },
+    //   {
+    //     fulfilled: (state, action) => {
+    //       const allTodolistTasks = state[action.payload.task.todoListId]
+    //       const taskIndex = allTodolistTasks.findIndex((task) => task.id === action.payload.task.id)
+    //       if (taskIndex !== -1) {
+    //         allTodolistTasks[taskIndex] = action.payload.task
+    //       }
+    //     },
+    //   },
+    // ),
+
+    changeTaskEntityStatusAC: create.reducer<{ todolistId: string; taskId: string; entityStatus: RequestStatus }>(
+      (state, action) => {
+        const tasks = state[action.payload.todolistId]
+        const task = tasks.find((t) => t.id === action.payload.taskId)
+        if (task) {
+          task.entityStatus = action.payload.entityStatus
+        }
       },
     ),
   }),
@@ -197,8 +214,10 @@ export const tasksSlice = createAppSlice({
   },
 })
 
-export const { fetchTasksTC, createTaskTC, deleteTaskTC, updateTaskTC } = tasksSlice.actions
+export const { fetchTasksTC, createTaskTC, deleteTaskTC, updateTaskTC, changeTaskEntityStatusAC } = tasksSlice.actions
 export const tasksReducer = tasksSlice.reducer
 export const { selectTasks } = tasksSlice.selectors
 
-export type TasksState = Record<string, DomainTask[]>
+// export type TasksState = Record<string, DomainTask[]>
+export type TasksState = Record<string, TaskWithStatus[]>
+export type TaskWithStatus = DomainTask & { entityStatus: RequestStatus }
--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
@@ -1,8 +1,7 @@
 import { EditableSpan } from "@/common/components/EditableSpan/EditableSpan"
 import { TaskStatus } from "@/common/enums"
 import { useAppDispatch } from "@/common/hooks"
-import type { DomainTask } from "@/features/todolists/api/tasksApi.types.ts"
-import { deleteTaskTC, updateTaskTC } from "@/features/todolists/model/tasks-slice.ts"
+import { deleteTaskTC, type TaskWithStatus, updateTaskTC } from "@/features/todolists/model/tasks-slice.ts"
 import type { DomainTodolist } from "@/features/todolists/model/todolists-slice.ts"
 import DeleteIcon from "@mui/icons-material/Delete"
 import Checkbox from "@mui/material/Checkbox"
@@ -13,7 +12,7 @@ import { getListItemSx } from "./TaskItem.styles"
 
 type Props = {
   todolist: DomainTodolist
-  task: DomainTask
+  task: TaskWithStatus
 }
 
 export const TaskItem = ({ task, todolist }: Props) => {
@@ -24,29 +23,29 @@ export const TaskItem = ({ task, todolist }: Props) => {
   }
 
   const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
-    // const newStatusValue = e.currentTarget.checked ? TaskStatus.Completed : TaskStatus.New
-    // const newTask = { ...task, status: newStatusValue }
-    // dispatch(updateTaskTC(newTask))
+    const newStatusValue = e.currentTarget.checked ? TaskStatus.Completed : TaskStatus.New
+    const newTask = { ...task, status: newStatusValue }
+    dispatch(updateTaskTC(newTask))
 
-    const newStatusValue = e.currentTarget.checked
-    dispatch(
-      updateTaskTC({
-        todolistId: todolist.id,
-        taskId: task.id,
-        domainModel: { status: newStatusValue ? TaskStatus.Completed : TaskStatus.New },
-      }),
-    )
+    // const newStatusValue = e.currentTarget.checked
+    // dispatch(
+    //   updateTaskTC({
+    //     todolistId: todolist.id,
+    //     taskId: task.id,
+    //     domainModel: { status: newStatusValue ? TaskStatus.Completed : TaskStatus.New },
+    //   }),
+    // )
   }
 
   const changeTaskTitle = (title: string) => {
-    // const newTask = { ...task, title }
-    // dispatch(updateTaskTC(newTask))
+    const newTask = { ...task, title }
+    dispatch(updateTaskTC(newTask))
 
-    dispatch(updateTaskTC({ todolistId: todolist.id, taskId: task.id, domainModel: { title } }))
+    // dispatch(updateTaskTC({ todolistId: todolist.id, taskId: task.id, domainModel: { title } }))
   }
 
   const isTaskCompleted = task.status === TaskStatus.Completed
-  const disabled = todolist.entityStatus === "loading"
+  const disabled = task.entityStatus === "loading"
 
   return (
     <ListItem sx={getListItemSx(isTaskCompleted)}>
