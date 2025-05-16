5ade53d01d6e28c88e4c969f6aaca2f663cc436f
PlatonKynin <kininplaton@gmail.com>
2025-05-16 22:51:34 +0300

    4 Спринт 3 неделя - Домашнее задание: 4. Реализация универсальной функции для работы с массивами с использованием дженериков (задача)

--- src/common/utils/genericsFn.ts
+++ src/common/utils/genericsFn.ts
@@ -70,3 +70,28 @@
 // const toDescription = (person: Person) => `${person.name} is ${person.age} years old`
 //
 // console.log(mapArray(people, toDescription)) // ["Agnes is 25 years old", "Robert is 30 years old"]
+
+
+// 4. Реализация универсальной функции для работы с массивами с использованием дженериков (задача)
+// Необходимо создать дженериковую функцию, принимающую массив любого типа и значение того же типа. Функция должна вернуть массив без изменений, если значение уже есть в массиве, а если нет - новый массив с добавленным значением.
+//
+// Требования:
+//
+// функция должна быть дженериковой и работать с массивами любого типа;
+// для проверки наличия элемента в массиве используйте метод includes;
+// типы массива и элемента должны быть связаны через дженерики;
+// функция должна быть чистой (не должна изменять оригинальный массив):
+
+const updateArray = <T,>(array: T[], value: T): T[] => {
+  return array.includes(value) ? array : [...array, value]
+}
+
+// Строки
+const stringArray = ["apple", "banana", "cherry"]
+console.log(updateArray(stringArray, "banana")) // ['apple', 'banana', 'cherry']
+console.log(updateArray(stringArray, "strawberry")) // ['apple', 'banana', 'cherry', 'strawberry']
+
+// Числа
+const numberArray = [1, 2, 3]
+console.log(updateArray(numberArray, 2)) // [1, 2, 3]
+console.log(updateArray(numberArray, 4)) // [1, 2, 3, 4]
