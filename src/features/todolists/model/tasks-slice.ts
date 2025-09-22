275eb146e8028367d0b35f0844b9bf3bc4f1393a
PlatonKynin <kininplaton@gmail.com>
2025-09-22 21:58:37 +0300

    relearn 4 sprint 2 week - changeTaskStatusTC решение через getState() и нахождения task var1

--- src/features/todolists/model/tasks-slice.ts
+++ src/features/todolists/model/tasks-slice.ts
@@ -1,7 +1,8 @@
+import type { RootState } from "@/app/store.ts"
 import { TaskStatus } from "@/common/enums"
 import { createAppSlice } from "@/common/utils"
 import { tasksApi } from "@/features/todolists/api/tasksApi.ts"
-import type { DomainTask } from "@/features/todolists/api/tasksApi.types.ts"
+import type { DomainTask, UpdateTaskModel } from "@/features/todolists/api/tasksApi.types.ts"
 import { createTodolistTC, deleteTodolistTC } from "./todolists-slice.ts"
 
 export const tasksSlice = createAppSlice({
@@ -65,12 +66,44 @@ export const tasksSlice = createAppSlice({
         },
       },
     ),
-    changeTaskStatusAC: create.reducer<{ todolistId: string; taskId: string; isDone: boolean }>((state, action) => {
-      const task = state[action.payload.todolistId].find((task) => task.id === action.payload.taskId)
-      if (task) {
-        task.status = action.payload.isDone ? TaskStatus.Completed : TaskStatus.New
-      }
-    }),
+    changeTaskStatusTC: create.asyncThunk(
+      async (payload: { todolistId: string; taskId: string; status: TaskStatus }, thunkAPI) => {
+        const { rejectWithValue, getState } = thunkAPI
+        const { todolistId, taskId, status } = payload
+
+        const allState = getState() as RootState
+        const allTodolistTasks = allState.tasks[todolistId]
+        const task = allTodolistTasks.find((task) => task.id === taskId)
+
+        if (!task) {
+          return thunkAPI.rejectWithValue(null)
+        }
+
+        const model: UpdateTaskModel = {
+          description: task.description,
+          title: task.title,
+          priority: task.priority,
+          startDate: task.startDate,
+          deadline: task.deadline,
+          status,
+        }
+        try {
+          const res = await tasksApi.updateTask({ todolistId, taskId, model })
+          return { task: res.data.data.item }
+        } catch (error) {
+          return rejectWithValue(null)
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          const { todoListId, id, status } = action.payload.task
+          const task = state[todoListId].find((task) => task.id === id)
+          if (task) {
+            task.status = status ? TaskStatus.Completed : TaskStatus.New
+          }
+        },
+      },
+    ),
     changeTaskTitleAC: create.reducer<{ todolistId: string; taskId: string; title: string }>((state, action) => {
       const task = state[action.payload.todolistId].find((task) => task.id === action.payload.taskId)
       if (task) {
@@ -89,7 +122,7 @@ export const tasksSlice = createAppSlice({
   },
 })
 
-export const { fetchTasksTC, createTaskTC, deleteTaskTC, changeTaskStatusAC, changeTaskTitleAC } = tasksSlice.actions
+export const { fetchTasksTC, createTaskTC, deleteTaskTC, changeTaskStatusTC, changeTaskTitleAC } = tasksSlice.actions
 export const tasksReducer = tasksSlice.reducer
 export const { selectTasks } = tasksSlice.selectors
 
--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/TaskItem/TaskItem.tsx
@@ -2,7 +2,7 @@ import { EditableSpan } from "@/common/components/EditableSpan/EditableSpan"
 import { TaskStatus } from "@/common/enums"
 import { useAppDispatch } from "@/common/hooks"
 import type { DomainTask } from "@/features/todolists/api/tasksApi.types.ts"
-import { changeTaskStatusAC, changeTaskTitleAC, deleteTaskTC } from "@/features/todolists/model/tasks-slice.ts"
+import { changeTaskStatusTC, changeTaskTitleAC, deleteTaskTC } from "@/features/todolists/model/tasks-slice.ts"
 import DeleteIcon from "@mui/icons-material/Delete"
 import Checkbox from "@mui/material/Checkbox"
 import IconButton from "@mui/material/IconButton"
@@ -23,8 +23,8 @@ export const TaskItem = ({ task, todolistId }: Props) => {
   }
 
   const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
-    const newStatusValue = e.currentTarget.checked
-    dispatch(changeTaskStatusAC({ todolistId, taskId: task.id, isDone: newStatusValue }))
+    const newStatusValue = e.currentTarget.checked ? TaskStatus.Completed : TaskStatus.New
+    dispatch(changeTaskStatusTC({ todolistId, taskId: task.id, status: newStatusValue }))
   }
 
   const changeTaskTitle = (title: string) => {
